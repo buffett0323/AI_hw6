4a1815b02a9a15c30ed26560b096fb4d71a1c64f
buffett0323 <philip910323@gmail.com>
2024-06-11 15:22:33 +0000

    Adding .gitignore

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ python main.py \
     --exp_name "${1}" \
     --model_name "${2}" \
     --train \
-    --wandb_token "0aad23b14e9c1e0e2342caaefbcf3c240a8a3e5e" \
+    --wandb_token "${3}" \
     --num_epochs 1 \
     --train_batch_size 4 \
     --eval_batch_size 4 \
